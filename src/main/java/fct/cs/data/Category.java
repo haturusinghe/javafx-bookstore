34db4e0222272f3e4be959a7003c29e29d67ded5
S.Haturusinghe <haturusingh3@gmail.com>
2021-08-28 15:05:55 +0530

    updated category data model

--- src/main/java/fct/cs/data/Category.java
+++ src/main/java/fct/cs/data/Category.java
@@ -3,10 +3,37 @@ package fct.cs.data;
 public class Category {
     private String category_id ;
     private String category_name;
+    private int low_value;
+    private int up_value;
+
+    public Category(String category_id, String category_name, int low_value, int up_value) {
+        this.category_id = category_id;
+        this.category_name = category_name;
+        this.low_value = low_value;
+        this.up_value = up_value;
+    }
 
     public Category(String category_id, String category_name) {
         this.category_id = category_id;
         this.category_name = category_name;
+        this.low_value = 0;
+        this.up_value = 100000;
+    }
+
+    public int getLow_value() {
+        return low_value;
+    }
+
+    public void setLow_value(int low_value) {
+        this.low_value = low_value;
+    }
+
+    public int getUp_value() {
+        return up_value;
+    }
+
+    public void setUp_value(int up_value) {
+        this.up_value = up_value;
     }
 
     public String getCategory_id() {
