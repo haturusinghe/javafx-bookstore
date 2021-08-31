28a4e73356d8791570a17992e89120b42440dfd1
S.Haturusinghe <haturusingh3@gmail.com>
2021-08-31 10:48:16 +0530

    added set and clear text field functions, added function to convert form text to stockEntry object , added update function

--- src/main/java/fct/cs/inventory/EditInventoryController.java
+++ src/main/java/fct/cs/inventory/EditInventoryController.java
@@ -11,6 +11,9 @@ public class EditInventoryController {
     private JFXDrawer drawer;
 
     @FXML
+    private TextField txtInvId;
+
+    @FXML
     private TextField txtBookId;
 
     @FXML
@@ -25,6 +28,41 @@ public class EditInventoryController {
     @FXML
     private Button cancelBtn;
 
+    @FXML
+    private Button submitBtn;
+
+    private InventoryManager inventoryManager;
+
+    private InventoryController parentController;
+
+    private boolean isAddingNew = false;
+
+
+    public void setTextFields(int inv_id ,int bookid,int price,int qty,int min){
+        txtInvId.setText(String.valueOf(inv_id));
+        txtBookId.setText(String.valueOf(bookid));
+        txtPrice.setText(String.valueOf(price));
+        txtQty.setText(String.valueOf(qty));
+        txtMin.setText(String.valueOf(min));
+    }
+
+    public void clearTextFields(){
+        txtInvId.clear();
+        txtBookId.clear();
+        txtPrice.setText("");
+        txtQty.setText("");
+        txtMin.setText("");
+    }
+
+    public StockEntry getStockEntryFromForm(){
+        int inv_id = Integer.parseInt(txtInvId.getText());
+        int book_id = Integer.parseInt(txtBookId.getText());
+        int price = Integer.parseInt(txtPrice.getText());
+        int qty = Integer.parseInt(txtQty.getText());
+        int min = Integer.parseInt(txtMin.getText());
+
+        return new StockEntry(inv_id,book_id,price,qty,min);
+    }
 
     public void cancel(ActionEvent actionEvent) {
         drawer.close();
@@ -32,5 +70,44 @@ public class EditInventoryController {
 
     public void setDrawer(JFXDrawer drawer) {
         this.drawer = drawer;
+    }
+
+    public void setParentController(InventoryController parentController) {
+        this.parentController = parentController;
+    }
+
+    public void setInventoryManager(InventoryManager inventoryManager) {
+        this.inventoryManager = inventoryManager;
+    }
+
+    public void setAddingNew(boolean addingNew) {
+        isAddingNew = addingNew;
+        txtInvId.setDisable(false);
+    }
+
+    public void updateInventory(ActionEvent actionEvent) {
+        if(!isAddingNew){
+            System.out.println(getStockEntryFromForm().toString());
+            inventoryManager.updateSingleEntry(getStockEntryFromForm());
+            clearTextFields();
+            parentController.loadTableData();
+            drawer.close();
+        }else{
+            inventoryManager.addSingleEntry(getStockEntryFromForm());
+            setAddingNew(false);
+            clearTextFields();
+            parentController.loadTableData();
+            drawer.close();
+        }
+    }
+
+    public void setEntry(StockEntry entry) {
+        setTextFields(
+                entry.getInv_id(),
+                entry.getBook_id(),
+                entry.getList_price(),
+                entry.getQty(),
+                entry.getMin_qty()
+                );
     }
 }
