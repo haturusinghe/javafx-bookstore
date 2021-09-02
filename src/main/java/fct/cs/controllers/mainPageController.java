fd67f6ef3421cdafb70340de95e018b94dc9cda4
Geethanjana <geethanjanayashoda@gmail.com>
2021-09-03 01:23:28 +0530

    add javafx event for run login scene to main page

--- src/main/java/fct/cs/controllers/mainPageController.java
+++ src/main/java/fct/cs/controllers/mainPageController.java
@@ -1,34 +1,19 @@
 package fct.cs.controllers;
 
 import com.jfoenix.controls.JFXButton;
-import fct.cs.Dash.DashController;
-import fct.cs.inventory.InventoryController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class mainPageController implements Initializable {
+public class mainPageController {
 
-    public JFXButton homePage;
-    @FXML
-    private Label pageHeaderLabel;
-
-    @FXML
-    private HBox header_hbox;
-
-    @FXML
-    private JFXButton inventoryPage;
-
+    public HBox header_hbox;
+    public JFXButton inventoryPage;
     @FXML
     private JFXButton orderBtn;
 
@@ -43,6 +28,7 @@ public class mainPageController implements Initializable {
 
     @FXML
     private VBox contentPanel_vbox;
+    
 
     @FXML
     private VBox contentAnchor;
@@ -51,26 +37,14 @@ public class mainPageController implements Initializable {
 
     private String currentPage = "";
 
-    private boolean isManager = true;
-
-    public void setManager(boolean manager) {
-        isManager = manager;
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadHomePage();
-    }
-
     @FXML
     void loadEmployeePage(ActionEvent event) {
         if (currentPage != "employees") {
-            loader = new FXMLLoader(getClass().getResource("/fct/cs/new-employee-page.fxml"));
+            loader = new FXMLLoader(getClass().getResource("/fct/cs/employee.fxml"));
             try {
                 mainContent_vbox.getChildren().clear();
                 mainContent_vbox.getChildren().add(loader.load());
                 currentPage = "employees";
-                setHeaderText("Manage Employees");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -80,13 +54,14 @@ public class mainPageController implements Initializable {
 //        EmployeeController childController = loader.getController();
     }
 
+
+
     public void loadOrdersPage(ActionEvent actionEvent) {
         if (!currentPage.equals("orders")) {
             loader = new FXMLLoader(getClass().getResource("/fct/cs/orders.fxml"));
             try {
                 mainContent_vbox.getChildren().clear();
                 mainContent_vbox.getChildren().add(loader.load());
-                setHeaderText("Manage Orders");
                 currentPage = "orders";
             } catch (IOException e) {
                 e.printStackTrace();
@@ -97,19 +72,12 @@ public class mainPageController implements Initializable {
     }
 
     public void loadInventoryPage(ActionEvent actionEvent) {
-       loadInventoryPage();
-    }
-
-    public void loadInventoryPage() {
         if (!currentPage.equals("inventory")) {
             loader = new FXMLLoader(getClass().getResource("/fct/cs/inventory.fxml"));
             try {
                 mainContent_vbox.getChildren().clear();
                 mainContent_vbox.getChildren().add(loader.load());
-                InventoryController inventoryController = loader.getController();
-                inventoryController.setManager(isManager);
                 currentPage = "inventory";
-                setHeaderText("Manage Inventory");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -120,36 +88,5 @@ public class mainPageController implements Initializable {
 
     public void exit(ActionEvent actionEvent) {
         System.exit(0);
-    }
-
-    public void setHeaderText(String string){
-        pageHeaderLabel.setText(string);
-        pageHeaderLabel.setStyle("-fx-font-family: Montserrat;");
-    }
-
-    public void loadHome(ActionEvent actionEvent) {
-        loadHomePage();
-
-    }
-
-    public void loadHomePage() {
-        if (!currentPage.equals("home")) {
-            loader = new FXMLLoader(getClass().getResource("/fct/cs/dash.fxml"));
-            try {
-                mainContent_vbox.getChildren().clear();
-                mainContent_vbox.getChildren().add(loader.load());
-                DashController controller = loader.getController();
-                controller.setManager(isManager);
-                currentPage = "home";
-                setHeaderText("Welcome");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }else{
-            System.out.println("already loaded");
-        }
-    }
-
-    public void loadBilling(ActionEvent actionEvent) {
     }
 }
