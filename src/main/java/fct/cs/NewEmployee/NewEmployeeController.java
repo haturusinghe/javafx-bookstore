81b79d67e26976bfe6725ea60f73b63f69a340e8
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-07 14:45:55 +0530

    added drawer control functions

--- src/main/java/fct/cs/NewEmployee/NewEmployeeController.java
+++ src/main/java/fct/cs/NewEmployee/NewEmployeeController.java
@@ -1,75 +1,56 @@
 package fct.cs.NewEmployee;
 
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.events.JFXDrawerEvent;
 import fct.cs.data.Category;
 import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.base.AbstractMFXDialog;
 import io.github.palexdev.materialfx.controls.cell.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.controls.enums.DialogType;
+import io.github.palexdev.materialfx.controls.factories.MFXDialogFactory;
 import io.github.palexdev.materialfx.font.MFXFontIcon;
-import javafx.beans.binding.Bindings;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXTableView;
-import io.github.palexdev.materialfx.controls.cell.MFXTableColumn;
-import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
-import io.github.palexdev.materialfx.controls.legacy.MFXLegacyTableView;
-import io.github.palexdev.materialfx.font.MFXFontIcon;
-import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import org.kordamp.ikonli.javafx.FontIcon;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Comparator;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.concurrent.Callable;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.ResourceBundle;
 
 public class NewEmployeeController implements Initializable {
     public MFXTableView empTable;
-
+    public JFXDrawer drawer;
+    private NewEmployeeController thisController = this;
     private ObservableList<EmployeeData> employeeObservableList = FXCollections.observableArrayList();
     private FilteredList<EmployeeData> employeeFilteredList = new FilteredList<>(employeeObservableList);
     private static EmployeeData selectedEmployee = null;
 
     private ObservableList<Category> categoryList = FXCollections.observableArrayList();
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         getEmployeeData();
         setColumnProps();
-
     }
 
-    private void getEmployeeData() {
+    public void getEmployeeData() {
         ArrayList<EmployeeData> eList = EmployeeManager.getEmployeeList(100, 1);
+        employeeObservableList.clear();
         for (EmployeeData e:
              eList) {
             employeeObservableList.add(e);
@@ -89,8 +70,8 @@ public class NewEmployeeController implements Initializable {
         MFXTableColumn<EmployeeData> fMameColumn = new MFXTableColumn<>("First Name", Comparator.comparing(EmployeeData::getFirst_name));
         MFXTableColumn<EmployeeData> lNameColumn = new MFXTableColumn<>("Last Name", Comparator.comparing(EmployeeData::getLast_name));
         MFXTableColumn<EmployeeData> salaryColumn = new MFXTableColumn<>("Salary", Comparator.comparing(EmployeeData::getSalary));
-        MFXTableColumn<EmployeeData> updateColumn = new MFXTableColumn<>("Update", Comparator.comparing(EmployeeData::getEmployee_id));
-        MFXTableColumn<EmployeeData> deleteColumn = new MFXTableColumn<>("Delete", Comparator.comparing(EmployeeData::getEmployee_id));
+        MFXTableColumn<EmployeeData> updateColumn = new MFXTableColumn<>("", Comparator.comparing(EmployeeData::getEmployee_id));
+        MFXTableColumn<EmployeeData> deleteColumn = new MFXTableColumn<>("", Comparator.comparing(EmployeeData::getEmployee_id));
 
         empIdColumn.setRowCellFunction(employee -> new MFXTableRowCell(String.valueOf(employee.getEmployee_id())));
         fMameColumn.setRowCellFunction(employee -> new MFXTableRowCell(String.valueOf(employee.getFirst_name())));
@@ -100,12 +81,41 @@ public class NewEmployeeController implements Initializable {
 //        updateColumn.setRowCellFunction(employee -> new MFXTableRowCell(String.valueOf(employee.getEmployee_id())));
 //        deleteColumn.setRowCellFunction(employee -> new MFXTableRowCell(String.valueOf(employee.getEmployee_id())));
 
-        updateColumn.setMinWidth(30);
+        updateColumn.setMinWidth(100);
         updateColumn.setShowLockIcon(false);
         updateColumn.setRowCellFunction(employeeData -> {
-            MFXTableRowCell rowCell = new MFXTableRowCell("");
-            rowCell.setGraphicTextGap(0);
-            rowCell.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> System.out.println("Update"));
+            MFXTableRowCell rowCell = new MFXTableRowCell("Update");
+            rowCell.setGraphicTextGap(5);
+            rowCell.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+                System.out.println("Update");
+
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fct/cs/employee-form.fxml"));
+                    VBox box = loader.load();
+                     EmployeeFormController controller = loader.getController();
+
+                    controller.setParentController(thisController);
+                    controller.setDrawer(drawer);
+//                    controller.setInventoryManager(inventoryManager);
+                    controller.setEntry(employeeData);
+                    controller.setAddingNew(false);
+                    drawer.setSidePane(box);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                if (drawer.isHidden()) {
+                    drawer.open();
+                    drawer.toFront();
+                    System.out.println("open");
+                } else {
+                    drawer.toBack();
+                    drawer.close();
+
+                    System.out.println("close");
+                }
+
+            });
             MFXFontIcon icon = new MFXFontIcon("mfx-file", 25);
 //            FontIcon icon = new FontIcon("antf-edit");
 //            icon.setIconSize(10);
@@ -117,11 +127,12 @@ public class NewEmployeeController implements Initializable {
             return rowCell;
         });
 
-        deleteColumn.setMinWidth(30);
+        deleteColumn.setMinWidth(100);
         deleteColumn.setShowLockIcon(false);
         deleteColumn.setRowCellFunction(employeeData -> {
-            MFXTableRowCell rowCell = new MFXTableRowCell("");
-            rowCell.setGraphicTextGap(0);
+            MFXTableRowCell rowCell = new MFXTableRowCell("Delete");
+            rowCell.setGraphicTextGap(5);
+//            rowCell.setStyle("-fx-background-color:grey");
             rowCell.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> System.out.println("Delete"));
             MFXFontIcon icon = new MFXFontIcon("mfx-minus-circle", 25);
 //            FontIcon icon = new FontIcon("antf-edit");
@@ -138,5 +149,9 @@ public class NewEmployeeController implements Initializable {
 
 
         empTable.getTableColumns().addAll(empIdColumn, fMameColumn,lNameColumn, salaryColumn,updateColumn,deleteColumn);
+    }
+
+    public void hideDrawer(JFXDrawerEvent jfxDrawerEvent) {
+        drawer.toBack();
     }
 }
