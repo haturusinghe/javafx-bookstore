d573a0f9b8c5592f709a38402b114b3609b5ddbd
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-07 12:47:00 +0530

    employee manager class used to handle employee data from database

---
+++ src/main/java/fct/cs/NewEmployee/EmployeeManager.java
@@ -0,0 +1,66 @@
+package fct.cs.NewEmployee;
+
+import fct.cs.dbUtil.DatabaseConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class EmployeeManager {
+    private static Connection conn = DatabaseConnector.getConn();
+
+    public static ArrayList<EmployeeData> getEmployeeList(int entriesPerPage, int pageNumber) {
+        ResultSet rs = getEmployeesFromDatabase(entriesPerPage, pageNumber);
+        return createEmployeeList(rs);
+    }
+
+    private static ResultSet getEmployeesFromDatabase(int entriesPerPage, int pageNumber) {
+        int offset = entriesPerPage * (pageNumber - 1);
+        String query = "SELECT * FROM employee LIMIT ?  OFFSET  ?";
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet;
+        try {
+            preparedStatement = conn.prepareStatement(query);
+
+            preparedStatement.setInt(1, entriesPerPage);
+            preparedStatement.setInt(2, offset);
+            resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static ArrayList<EmployeeData> createEmployeeList(ResultSet rs) {
+        ArrayList<EmployeeData> orderList = new ArrayList<>();
+        try {
+            //
+            //employee_id
+            //first_name
+            //last_name
+            //email
+            //gender
+            //phone_number
+            //salary
+            while (rs.next()) {
+                orderList.add(new EmployeeData(
+                        String.valueOf(rs.getInt("employee_id")),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("email"),
+                        rs.getString("gender"),
+                        rs.getString("phone_number"),
+                        rs.getInt("salary")
+
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return orderList;
+    }
+}
