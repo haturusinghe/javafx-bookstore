373c1f17813f4e71720392950a782ac630d8225e
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-09 12:48:39 +0530

    initialize the Customer

---
+++ src/main/java/fct/cs/data/Customer.java
@@ -0,0 +1,12 @@
+package fct.cs.data;
+
+public class Customer {
+    private int customer_id;
+    private String customer_name;
+    private String location;
+    private String email;
+    private int min_qty;
+    private boolean isLow;
+
+    
+}
