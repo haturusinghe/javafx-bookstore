783cdf6bd4cd93c75e2f936a1397a1de5232759f
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-09 13:39:22 +0530

    ui teaks

--- src/main/java/fct/cs/App.java
+++ src/main/java/fct/cs/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Register"),1280,720);
+        scene = new Scene(loadFXML("login"),1280,720);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
