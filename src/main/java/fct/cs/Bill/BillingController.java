6e563a32f03558ad2226093a7d1fc77f6386e3c2
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-09 18:22:57 +0530

    getting Billing variables

--- src/main/java/fct/cs/Bill/BillingController.java
+++ src/main/java/fct/cs/Bill/BillingController.java
@@ -1,4 +1,41 @@
 package fct.cs.Bill;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+
 public class BillingController {
+
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private Label customer;
+
+    @FXML
+    private TableColumn<?, ?> billTable;
+
+    @FXML
+    private Label total;
+
+    @FXML
+    private TextField discount;
+
+    @FXML
+    private Label finalTotal;
+
+    @FXML
+    private Button chargeCustomer;
+
+    @FXML
+    private Button cancel;
+
+
 }
