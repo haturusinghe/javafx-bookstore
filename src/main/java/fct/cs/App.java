c395efac597027d0b536a076cd492e4e1ff6e913
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-09 13:22:41 +0530

    some fixes

--- src/main/java/fct/cs/App.java
+++ src/main/java/fct/cs/App.java
@@ -1,6 +1,5 @@
 package fct.cs;
 
-import fct.cs.commonUtil.AppUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -17,13 +16,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("main-dash"),1280,720);
-        scene.getStylesheets().add(
-                "https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Inconsolata:wght@300&family=Josefin+Sans&family=Montserrat:wght@300;400&family=Teko&family=Work+Sans&display=swap");
-//        scene.getStylesheets().add("https://fonts.googleapis.com/css?family=Inconsolata:700");
-        //'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap'
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
-        AppUtils.enableDrag(scene,stage);
         stage.show();
 
     }
