41dad345a5e5d0a3d265338c13f6a2f19c43620c
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-10 01:28:52 +0530

    modify constructor BillCustomer.java

--- src/main/java/fct/cs/Bill/BillCustomer.java
+++ src/main/java/fct/cs/Bill/BillCustomer.java
@@ -1,12 +1,18 @@
 package fct.cs.Bill;
 
 public class BillCustomer {
-    private int customer_id ; 
+    private int customer_id ;
+
+    public BillCustomer(int customer_id, String customer_name, String email, String mobile) {
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
     private String customer_name,email,mobile ;
 
-    public BillCustomer(String customer_name) {
-        this.customer_name = customer_name;
-    }
+
 
     public String getCustomer_name() {
         return customer_name;
