7a54df439fd5fa96823f3b56ce18baad3c2d5ca5
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-09 12:54:28 +0530

    create Customer constructor

--- src/main/java/fct/cs/data/Customer.java
+++ src/main/java/fct/cs/data/Customer.java
@@ -4,9 +4,16 @@ public class Customer {
     private int customer_id;
     private String customer_name;
     private String location;
-    private String email;
-    private int min_qty;
-    private boolean isLow;
+    private int mobile;
 
-    
+    private String email;
+
+    public Customer(int customer_id, String customer_name,String mobile , String location, String email){
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+        this.location = location;
+        this.email = email;
+    }
 }
+
+
