4f8f0d92d38bc8073a8c515d32a3e7442dc09b37
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-10 01:29:52 +0530

    modify getters and setters BillCustomer.java

--- src/main/java/fct/cs/Bill/BillCustomer.java
+++ src/main/java/fct/cs/Bill/BillCustomer.java
@@ -2,6 +2,7 @@ package fct.cs.Bill;
 
 public class BillCustomer {
     private int customer_id ;
+    private String customer_name,email,mobile ;
 
     public BillCustomer(int customer_id, String customer_name, String email, String mobile) {
         this.customer_id = customer_id;
@@ -10,9 +11,29 @@ public class BillCustomer {
         this.mobile = mobile;
     }
 
-    private String customer_name,email,mobile ;
+    public int getCustomer_id() {
+        return customer_id;
+    }
 
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 
     public String getCustomer_name() {
         return customer_name;
