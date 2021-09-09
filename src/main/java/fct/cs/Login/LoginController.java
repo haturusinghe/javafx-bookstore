111f451d91c9602661b77ac0c7ec418589233d7e
Geethanjana <geethanjanayashoda@gmail.com>
2021-09-09 19:03:38 +0530

    PasswordSecure test run pass

--- src/main/java/fct/cs/Login/LoginController.java
+++ src/main/java/fct/cs/Login/LoginController.java
@@ -4,29 +4,32 @@ package fct.cs.Login;
 
 
 
-        import java.io.IOException;
-        import java.net.URL;
-        import java.sql.*;
-        import java.util.ResourceBundle;
+import java.io.IOException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-        import com.jfoenix.controls.JFXButton;
-        import fct.cs.dbUtil.DatabaseConnector;
-        import io.github.palexdev.materialfx.controls.MFXPasswordField;
-        import io.github.palexdev.materialfx.controls.MFXTextField;
-        import javafx.application.Platform;
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.fxml.Initializable;
-        import javafx.scene.Node;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.*;
-        import javafx.scene.image.ImageView;
-        import javafx.stage.Stage;
+import com.jfoenix.controls.JFXButton;
+import fct.cs.dbUtil.DatabaseConnector;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.utils.BindingUtils;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-        import javafx.scene.image.Image;
-        import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -58,6 +61,9 @@ public class LoginController implements Initializable {
 
     @FXML
     private Button exitBtn;
+
+    @FXML
+    private Hyperlink registerOnAction;
 
 
     private Connection conn;
@@ -99,10 +105,11 @@ public class LoginController implements Initializable {
 
 
     @FXML
-    void loginOnAction(ActionEvent event) throws IOException {
+    void loginOnAction(ActionEvent event) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
 
 
         DatabaseConnector databaseConnector = new DatabaseConnector();
+        PasswordSecure decrypt = new PasswordSecure();
 
         try {
             this.conn = databaseConnector.getConn();
@@ -110,55 +117,71 @@ public class LoginController implements Initializable {
             String username = txtUsername.getText().trim();
             String password = txtPass.getText().trim();
 
-            if(username.isEmpty() || password.isEmpty()){
-//                errorMsg.setText("Please insert username and password");
+
+
+
+            if (username.isEmpty() || password.isEmpty()) {
+
+                txtUsername.setValidated(true);
+                txtUsername.getValidator().add(
+                        BindingUtils.toProperty(
+                                txtUsername.textProperty().length().isNotEqualTo(0)
+                        ),
+                        "You need enter username"
+                );
+
+                txtPass.setValidated(true);
+                txtPass.getValidator().add(
+                        BindingUtils.toProperty(
+                                txtPass.textProperty().length().isNotEqualTo(0)
+                        ),
+                        "You need enter password"
+                );
+
+                System.out.println("Empty field");
             }
-            else
-            {
+            else {
                 //sql query for getting username as telnum
-                PreparedStatement ps_1 = conn.prepareStatement("select * from login where telnum=?"
-                        + " and password=?");
+                PreparedStatement ps_1 = conn.prepareStatement("select * from login where telnum=?");
                 //sql query for getting username as email
-                PreparedStatement ps_2 = conn.prepareStatement("select * from login where email=?"
-                        + " and password=?");
+                PreparedStatement ps_2 = conn.prepareStatement("select * from login where email=?");
 
-                ps_1.setString(1,txtUsername.getText().trim() );
-                ps_1.setString(2, txtPass.getText().trim());
-
-                ps_2.setString(1,txtUsername.getText().trim() );
-                ps_2.setString(2, txtPass.getText().trim());
+                ps_1.setString(1, username);
+                ps_2.setString(1, username);
 
                 ResultSet rs_1 = ps_1.executeQuery();
                 ResultSet rs_2 = ps_2.executeQuery();
 
-                if(rs_1.next() || rs_2.next()){
-                    System.out.println("Found");
-                    Parent view = FXMLLoader.load(getClass().getResource("/fct/cs/Register.fxml"));
-                    Scene scene = new Scene(view);
-                    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-                    window.setScene(scene);
-                    window.show();
+                if(rs_1.next()){
+                    String storedPassword = rs_1.getString("password");
+                    boolean matched = decrypt.validatePassword(password, storedPassword );
+                    System.out.println(matched);
 
 
-                }
+                }else if(rs_2.next()){
+                    String storedPassword = rs_2.getString("password");
+                    boolean matched = decrypt.validatePassword(password, storedPassword );
+                    System.out.println(matched);
 
-                else {
+                } else {
                     System.out.println("Not Found");
 //                    errorMsg.setText("Invalid credentials. Please try again");
                 }
             }
-        }catch(Exception ex){
+        } catch (SQLException throwables) {
 //            System.out.println("error" + ex.toString());
-            ex.printStackTrace();
+            throwables.printStackTrace();
         }
+
     }
 
+    //Load register.fxml
     @FXML
-    public void signupScene(ActionEvent event)throws IOException{
-        Parent view3=FXMLLoader.load(getClass().getResource("SignupPage.fxml"));
-        Scene scene3=new Scene(view3);
-        Stage window =new Stage();
-        window.setScene(scene3);
+    public void registerOnAction(ActionEvent event)throws IOException{
+        Parent view = FXMLLoader.load(getClass().getResource("/fct/cs/Register.fxml"));
+        Scene scene = new Scene(view);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
         window.show();
     }
 
--- src/main/java/fct/cs/Login/PasswordSecure.java
+++ src/main/java/fct/cs/Login/PasswordSecure.java
@@ -3,127 +3,71 @@ package fct.cs.Login;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 
 public class PasswordSecure {
 
-/*public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException
-        {
-        String  originalPassword = "password";
-        String generatedSecuredPasswordHash = generateStorngPasswordHash(originalPassword);
-        System.out.println(generatedSecuredPasswordHash);
-        }*/
-
     //encryption
-    static String encryptString(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        int iterations = 1000;
-        char[] chars = password.toCharArray();
-        byte[] salt = getSalt();
+        static String encryptString(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+            int iterations = 1000;
+            char[] chars = password.toCharArray();
+            byte[] salt = getSalt();
 
-        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hash = skf.generateSecret(spec).getEncoded();
-        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
-    }
-
-    private static byte[] getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
-    }
-
-    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
-
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-        if(paddingLength > 0) {
-        return String.format("%0"  +paddingLength + "d", 0) + hex;
-        }else{
-        return hex;
+            PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = skf.generateSecret(spec).getEncoded();
+            return iterations + ":" + toHex(salt) + ":" + toHex(hash);
         }
-    }
+
+        private static byte[] getSalt() throws NoSuchAlgorithmException {
+            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+            byte[] salt = new byte[16];
+            sr.nextBytes(salt);
+            return salt;
+        }
+
+        private static String toHex(byte[] array) throws NoSuchAlgorithmException {
+
+            BigInteger bi = new BigInteger(1, array);
+            String hex = bi.toString(16);
+            int paddingLength = (array.length * 2) - hex.length();
+            if(paddingLength > 0) {
+                return String.format("%0"  +paddingLength + "d", 0) + hex;
+            }else{
+                return hex;
+            }
+        }
 
 
     //Decryption
-    private static boolean validatePassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException
-    {
-        String[] parts = storedPassword.split(":");
-        int iterations = Integer.parseInt(parts[0]);
-        byte[] salt = fromHex(parts[1]);
-        byte[] hash = fromHex(parts[2]);
+        static boolean validatePassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+            String[] parts = storedPassword.split(":");
+            int iterations = Integer.parseInt(parts[0]);
+            byte[] salt = fromHex(parts[1]);
+            byte[] hash = fromHex(parts[2]);
 
-        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] testHash = skf.generateSecret(spec).getEncoded();
+            PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] testHash = skf.generateSecret(spec).getEncoded();
 
-        int diff = hash.length ^ testHash.length;
-        for(int i = 0; i < hash.length && i < testHash.length; i++)
-        {
-            diff |= hash[i] ^ testHash[i];
-        }
-        return diff == 0;
-    }
-    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
-    {
-        byte[] bytes = new byte[hex.length() / 2];
-        for(int i = 0; i<bytes.length ;i++)
-        {
-            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
-        }
-        return bytes;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    /*public static String getSecurePassword(String password, byte[] salt) {
-
-        String generatedPassword = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt);
-            byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            int diff = hash.length ^ testHash.length;
+            for(int i = 0; i < hash.length && i < testHash.length; i++)
+            {
+                diff |= hash[i] ^ testHash[i];
             }
-            generatedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            return diff == 0;
         }
-        return generatedPassword;
-    }
 
-    static byte[] getSalt() throws NoSuchAlgorithmException {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        return salt;
-    }
-
-    /*public static void main(String[] args) throws NoSuchAlgorithmException {
-        // same salt should be passed
-        byte[] salt = getSalt();
-        String password1 = getSecurePassword("Password", salt);
-        String password2 = getSecurePassword("Password", salt);
-        System.out.println(" Password 1 -> " + password1);
-        System.out.println(" Password 2 -> " + password2);
-        if (password1.equals(password2)) {
-            System.out.println("passwords are equal");
+        private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+        {
+            byte[] bytes = new byte[hex.length() / 2];
+            for(int i = 0; i<bytes.length ;i++)
+            {
+                bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+            }
+            return bytes;
         }
-    }*/
+
 }
