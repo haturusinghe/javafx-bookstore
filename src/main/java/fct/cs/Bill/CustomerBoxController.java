8d03ec080d4c5e9adb1436a57fb233461d1dddde
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-12 19:34:25 +0530

    initialize customer box class

---
+++ src/main/java/fct/cs/Bill/CustomerBoxController.java
@@ -0,0 +1,26 @@
+package fct.cs.Bill;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class CustomerBoxController {
+
+        @FXML
+        private Label id;
+
+        @FXML
+        private Label name;
+
+        @FXML
+        private Label mobile;
+
+        @FXML
+        private Label email;
+
+
+       
+
+}
+
