9b0a03670890d269076409b66add3a842cc2ef69
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-12 11:04:16 +0530

    reworked Create Account Function

--- src/main/java/fct/cs/Login/RegisterController.java
+++ src/main/java/fct/cs/Login/RegisterController.java
@@ -5,6 +5,9 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import fct.cs.dbUtil.DatabaseConnector;
 import io.github.palexdev.materialfx.controls.*;
+import io.github.palexdev.materialfx.controls.base.AbstractMFXDialog;
+import io.github.palexdev.materialfx.controls.enums.DialogType;
+import io.github.palexdev.materialfx.controls.factories.MFXDialogFactory;
 import io.github.palexdev.materialfx.utils.BindingUtils;
 import io.github.palexdev.materialfx.utils.StringUtils;
 import javafx.beans.binding.Bindings;
@@ -26,6 +29,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.Label;
@@ -127,7 +131,18 @@ public class RegisterController implements Initializable {
                 "You need enter your email address"
         );
 
-        //check passwaordget validation
+        emailAddress.setValidated(true);
+        emailAddress.getValidator().add(BindingUtils.toProperty(
+                Bindings.createBooleanBinding(
+                        () -> StringUtils.containsAll(emailAddress.getText(),
+                                "",  "@", "."),
+                        emailAddress.textProperty()
+                )
+                ),
+                "You must enter valid Email Address"
+        );
+
+
         passwordGet.setValidated(true);
         passwordGet.getValidator().add(
                 BindingUtils.toProperty(
@@ -136,25 +151,7 @@ public class RegisterController implements Initializable {
                 "You need enter your password"
         );
 
-        //check passwaordcheck validation
-        passwordCheck.setValidated(true);
-        passwordCheck.getValidator().add(
-                BindingUtils.toProperty(
-                        passwordCheck.passwordProperty().length().isNotEqualTo(0)
-                ),
-                "You need enter your password Again"
-        );
 
-        emailAddress.setValidated(true);
-        emailAddress.getValidator().add(BindingUtils.toProperty(
-                        Bindings.createBooleanBinding(
-                                () -> StringUtils.containsAll(emailAddress.getText(),
-                                        "",  "@", "."),
-                                emailAddress.textProperty()
-                        )
-                ),
-                "You must enter valid Email Address"
-        );
 
         passwordGet.setValidated(true);
         passwordGet.getValidator().add(
@@ -182,13 +179,18 @@ public class RegisterController implements Initializable {
         );
 
 
+        passwordCheck.setValidated(true);
+        passwordCheck.getValidator().add(
+                BindingUtils.toProperty(
+                        passwordCheck.passwordProperty().length().isNotEqualTo(0)
+                ),
+                "You need re-enter your password"
+        );
 
     }
 
     private Connection conn;
     //private DatabaseConnector databaseConnector;
-
-
 
     @FXML
     public void signOnAction(ActionEvent event) throws NoSuchAlgorithmException, InvalidKeySpecException {
@@ -291,6 +293,72 @@ public class RegisterController implements Initializable {
         window.show();
     }
 
+    public void createAccount(ActionEvent actionEvent) {
+        String fName = firstName.getText().trim();
+        String lName = lastName.getText().trim();
+        String pNum = telNum.getText().trim();
+        String email = emailAddress.getText().trim();
+        String passGet = passwordGet.getText().trim();
+        String passCheck = passwordCheck.getText().trim();
+        String answer = ansField.getText().trim();
+        String question = quesBox.getValue().toString();
+
+        passwordCheck.setValidated(true);
+        passwordCheck.getValidator().add(
+                BindingUtils.toProperty(
+                        passwordCheck.passwordProperty().isEqualTo(passwordGet.getPassword())
+                ),
+                "Passwords dont match"
+        );
 
 
+        if (firstName.isValid() && lastName.isValid() && telNum.isValid() && emailAddress.isValid() &&
+                passwordGet.isValid() && passwordCheck.isValid() &&
+                ansField.isValid() && quesBox.getValue().toString().length() > 5
+        ) {
+            System.out.println(passwordGet.passwordProperty().getValue());
+            System.out.println(passwordGet.passwordProperty().getValueSafe());
+            System.out.println("Form OK!");
+            DatabaseConnector databaseConnector = new DatabaseConnector();
+            this.conn = databaseConnector.getConn();
+            PasswordSecure encrypt = new PasswordSecure();
+            String passwordEncrypt = null, answerEncrypt = null;
+            //Encrypt password
+            try {
+                passwordEncrypt = encrypt.encryptString(passGet);
+                System.out.println("Password Encrypted");
+                answerEncrypt = encrypt.encryptString(answer);
+                System.out.println("Answer Encrypted");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (InvalidKeySpecException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (passwordEncrypt != null && answerEncrypt != null) {
+                    String sql = "INSERT INTO login (fname, lname, telnum, email, password, ques, ans) values(?,?,?,?,?,?,?)";
+                    PreparedStatement ps_3 = conn.prepareStatement(sql);
+
+                    ps_3.setString(1, fName);
+                    ps_3.setString(2, lName);
+                    ps_3.setString(3, pNum);
+                    ps_3.setString(4, email);
+                    ps_3.setString(5, passwordEncrypt);
+                    ps_3.setString(6, question);
+                    ps_3.setString(7, answerEncrypt);
+
+                    ps_3.execute();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Account successfully registered");
+
+        } else {
+            System.out.println("Check Again");
+        }
+
+    }
 }
