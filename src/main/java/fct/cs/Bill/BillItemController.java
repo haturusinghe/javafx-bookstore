462399dcb456480e23f5632704db4078130c5484
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-13 14:24:06 +0530

    add button for items

--- src/main/java/fct/cs/Bill/BillItemController.java
+++ src/main/java/fct/cs/Bill/BillItemController.java
@@ -3,12 +3,17 @@ package fct.cs.Bill;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -66,5 +71,58 @@ public class BillItemController implements Initializable {
         isbn.setCellValueFactory(new PropertyValueFactory<>("isbn"));
         unit_price.setCellValueFactory(new PropertyValueFactory<>("unit_price"));
 
-    }
+
+
+     additem.setCellFactory((tableColumn) -> {
+        TableCell<BillCustomer, Integer> tableCell = new TableCell<>() {
+            javafx.scene.image.Image imgSelect = new Image(getClass().getResourceAsStream("/images/book.png"));
+            final javafx.scene.control.Button btnAdd = new Button();
+            FontIcon icon3 = new FontIcon("antf-book");
+
+
+            @Override
+            protected void updateItem(Integer customer_id, boolean empty) {
+                super.updateItem(customer_id, empty);
+
+                if(empty)
+                {
+                    this.setText(null);
+                    this.setGraphic(null);
+                }
+                else{
+
+                    btnAdd.setOnAction(e ->{
+
+
+
+                    });
+
+
+                    btnAdd.setStyle("-fx-background-color: transparent;");
+
+                    icon3.setIconColor(Color.RED);
+                    icon3.setIconSize(30);
+
+                    ImageView iv = new ImageView();
+                    iv.setImage(imgSelect);
+                    iv.setPreserveRatio(true);
+                    iv.setSmooth(true);
+                    iv.setCache(true);
+                    btnAdd.setGraphic(icon3);
+
+                    this.setGraphic(btnAdd);
+                    this.setAlignment(Pos.CENTER);
+
+
+                }
+
+
+            }
+
+        };
+        return tableCell;
+    });
+
+
+}
 }
