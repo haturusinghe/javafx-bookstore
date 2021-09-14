aebd361324c1fb1bc52c527b93dd12e8e4b25123
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-14 23:38:45 +0530

    add single entry to update order details table

--- src/main/java/fct/cs/Bill/BillManager.java
+++ src/main/java/fct/cs/Bill/BillManager.java
@@ -1,9 +1,12 @@
 package fct.cs.Bill;
 
+import fct.cs.data.Order;
+import fct.cs.data.OrderDetailEntry;
 import fct.cs.dbUtil.DatabaseConnector;
 import fct.cs.dbUtil.DatabaseHandler;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BillManager {
@@ -16,7 +19,24 @@ public class BillManager {
         this.conn = databaseConnector.getConn();
     }
 
+    public boolean addSingleEntry(OrderDetailEntry entry){
+        String addQuery = "insert into order_details (order_id, book_id, quantity, unit_price, total_price , order_date) values (?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = null;
+        int count = 0;
+        try {
+            preparedStatement = conn.prepareStatement(addQuery);
+            preparedStatement.setInt(1,entry.getOrder_id());
+            preparedStatement.setInt(2,entry.getBook_id());
+            preparedStatement.setInt(3,entry.getQuantity());
+            preparedStatement.setInt(4,entry.getUnit_price());
+            preparedStatement.setInt(5,entry.getTotalForItem());
+            preparedStatement.setInt(5,entry.getTotalForItem());
+            count = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-
+        return count > 0;
+    }
 
 }
