7921f5fa3acad7de448641d0d2f27c66c42d2453
Nandun Perera <pererawn-cs18032@stu.kln.ac.lk>
2021-09-14 23:24:15 +0530

    create DB connection for BillManager

--- src/main/java/fct/cs/Bill/BillManager.java
+++ src/main/java/fct/cs/Bill/BillManager.java
@@ -1,5 +1,22 @@
 package fct.cs.Bill;
 
+import fct.cs.dbUtil.DatabaseConnector;
+import fct.cs.dbUtil.DatabaseHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class BillManager {
-    
+    private DatabaseConnector databaseConnector;
+    private Connection conn;
+
+    public BillManager() {
+
+        databaseConnector = new DatabaseConnector();
+        this.conn = databaseConnector.getConn();
+    }
+
+
+
+
 }
