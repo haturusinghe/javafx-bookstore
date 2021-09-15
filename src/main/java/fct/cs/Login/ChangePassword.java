62d63f495fa5fffd74fc7b6b67a509a032f08206
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-15 08:51:02 +0530

    fixed errors

--- src/main/java/fct/cs/Login/ChangePassword.java
+++ src/main/java/fct/cs/Login/ChangePassword.java
@@ -192,9 +192,11 @@ public class ChangePassword implements Initializable {
                                 Parent view = FXMLLoader.load(getClass().getResource("/fct/cs/login.fxml"));
                                 Scene scene = new Scene(view);
                                 System.out.println("Load Login Page");
-                                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                               /* Stage window = event.getClass().get
                                 window.setScene(scene);
-                                window.show();
+                                window.show();*/
+
+                            }catch (Exception e){
 
                             }
                         }
@@ -208,9 +210,6 @@ public class ChangePassword implements Initializable {
                 }
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
-            }catch (IOException e) {
-                e.printStackTrace();
-
             }
         } else {
 
@@ -255,5 +254,106 @@ public class ChangePassword implements Initializable {
     }
 
 
+    public void changeOnAction(ActionEvent actionEvent) {
+        String username = fUsername.getText().trim();
+        String answer = fAns.getText().trim();
+        String passGet = fPasswordGet.getText().trim();
+        String passCheck = fPasswordCheck.getText().trim();
+        String question = null;
+        try {
+            question = fQuesBox.getValue().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        fPasswordCheck.setValidated(true);
+        fPasswordCheck.getValidator().add(
+                BindingUtils.toProperty(
+                        fPasswordCheck.passwordProperty().isEqualTo(fPasswordGet.getPassword())
+                ),
+                "Passwords dont match"
+        );
+
+        if (fUsername.isValid() && fAns.isValid() && fPasswordGet.isValid() && fPasswordCheck.isValid() && question != null) {
+            System.out.println(fPasswordGet.passwordProperty().getValue());
+            System.out.println(fPasswordGet.passwordProperty().getValueSafe());
+            System.out.println("Form OK!");
+
+            try {
+                this.conn = DatabaseHandler.getInstance().getConn();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            PasswordSecure encrypt = new PasswordSecure();
+            PasswordSecure decrypt = new PasswordSecure();
+            String passwordEncrypt = null, answerDecrypt = null;
+
+            //Encrypt password
+            try {
+                passwordEncrypt = encrypt.encryptString(passGet);
+                System.out.println("Password Encrypted");
+
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (InvalidKeySpecException e) {
+                e.printStackTrace();
+            }
+
+            try {
+
+                //sql query for getting username as telnum
+                PreparedStatement ps_1 = conn.prepareStatement("select * from login where telnum=?");
+                //sql query for getting username as email
+                PreparedStatement ps_2 = conn.prepareStatement("select * from login where email=?");
+
+                ps_1.setString(1, username);
+                ps_2.setString(1, username);
+
+                ResultSet rs_1 = ps_1.executeQuery();
+                ResultSet rs_2 = ps_2.executeQuery();
+
+                if (rs_1.next()) {
+                    String storedAnswer = rs_1.getString("ans");
+                    Integer id = rs_1.getInt(1);
+                    boolean matchedAnswer = decrypt.validateString(answer, storedAnswer);
+                    boolean changePass = correctAns(passwordEncrypt, id);
+
+                    if (matchedAnswer == true) {
+
+                        if (changePass == true) {
+
+                            try {
+
+                                Parent view = FXMLLoader.load(getClass().getResource("/fct/cs/login.fxml"));
+                                Scene scene = new Scene(view);
+                                System.out.println("Load Login Page");
+//                               Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                                Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                                window.setScene(scene);
+                                window.show();
+
+                            } catch (Exception e) {
+
+                            }
+                        }
+
+
+                    } else {
+
+
+                    }
+
+                }
+            } catch (SQLException | NoSuchAlgorithmException | InvalidKeySpecException throwables) {
+                throwables.printStackTrace();
+            }
+        } else {
+
+            System.out.println("Check Again");
+            MFXStageDialog dialog = new MFXStageDialog(DialogType.WARNING, "Fill Forget-Password Form", "Please fill all the fields in the above form");
+            dialog.show();
+
+        }
+    }
 }
