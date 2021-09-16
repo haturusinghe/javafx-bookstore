1bae11fc354b9d239a6a61b30394587f50c703b7
Sanjula Thilakarathne <sanjkulaprsan@gmail.com>
2021-09-16 10:09:25 +0530

    change hbox

--- src/main/java/fct/cs/book/MainBookController.java
+++ src/main/java/fct/cs/book/MainBookController.java
@@ -1,12 +1,14 @@
 package fct.cs.book;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.events.JFXDrawerEvent;
-import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.*;
 import io.github.palexdev.materialfx.controls.cell.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.controls.enums.Styles;
 import io.github.palexdev.materialfx.font.MFXFontIcon;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,6 +17,7 @@ import javafx.geometry.Pos;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,12 +30,78 @@ public class MainBookController implements Initializable {
     public JFXDrawer drawer;
     private MainBookController thisController = this;
     private ObservableList<BookData> bookObservableList = FXCollections.observableArrayList();
+    private FilteredList<BookData> bookFilteredList;
+
+    private static BookData selectedBook = null;
+    private final MFXTextField searchField = new MFXTextField();
+    private final MFXComboBox<String> searchCombo = new MFXComboBox<>();
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         getBookData();
         setColumnProps();
+
+        bookTable.setHeaderSupplier(() -> {
+            HBox mainContainer = new HBox();
+            mainContainer.setPrefHeight(50);
+            mainContainer.setPrefWidth(1030);
+            mainContainer.setAlignment(Pos.CENTER_LEFT);
+            mainContainer.setPadding(new Insets(10,0,5,5));
+
+            HBox spanMid = new HBox();
+            spanMid.setMinWidth(610);
+            spanMid.setMinHeight(50);
+
+            HBox spanEnd = new HBox();
+            spanEnd.setMinHeight(50);
+            spanEnd.setMaxWidth(10);
+
+            HBox searchContainer = new HBox(10);
+            searchContainer.setMinHeight(50);
+            searchContainer.setAlignment(Pos.CENTER_RIGHT);
+            searchField.setPromptText("Search Employees");
+            searchField.setIcon(new MFXIconWrapper(new MFXFontIcon("mfx-search", 28, Color.web("#4D4D4D")), 24));
+            searchField.setIconInsets(new Insets(0,0,10,0));
+            searchField.setMinHeight(50);
+            searchField.setStyle("-fx-font-size: 18px;");
+
+            searchField.setOnKeyTyped(actionEvent -> {
+                bookFilteredList = new FilteredList<BookData>(bookObservableList);
+                thisController.searchTableFromText(searchField.getText());
+                bookTable.setItems(bookFilteredList);
+            });
+
+            searchCombo.setItems(FXCollections.observableArrayList("Name", "Salary", "Gender"));
+            searchCombo.setPromptText("Select Category");
+            searchCombo.setMinHeight(50);
+            searchCombo.setComboStyle(Styles.ComboBoxStyles.STYLE2);
+            searchCombo.setStyle("-fx-font-size: 30px;");
+
+            FontIcon addIcon = new FontIcon("anto-plus-circle");
+            addIcon.setIconColor(Color.WHITE);
+            addIcon.setIconSize(25);
+
+            MFXButton addBtn = new MFXButton();
+            addBtn.setText("Add Employee");
+            addBtn.setStyle("-fx-background-color: #2B2B2B;-fx-font-size: 20px;-fx-background-radius: 9,8,5,4,3;-fx-text-fill: #fff;");
+            addBtn.setGraphic(addIcon);
+            addBtn.setOnAction(actionEvent -> {
+                thisController.addNewBook();
+            });
+
+
+            /*MFXLabel testLabel = new MFXLabel("Header");
+            HBox holderHbox = new HBox();
+            holderHbox.getChildren().addAll(testLabel);
+            holderHbox.setMaxWidth(Region.USE_PREF_SIZE);
+            VBox box = new VBox(holderHbox);
+            box.setAlignment(Pos.CENTER_RIGHT);*/
+
+            searchContainer.getChildren().addAll(searchField);
+            mainContainer.getChildren().addAll(searchContainer,spanMid,addBtn,spanEnd);
+            return mainContainer;
+        });
     }
 
     public void getBookData() {
@@ -139,7 +208,7 @@ public class MainBookController implements Initializable {
         drawer.toBack();
     }
 
-    public void addNewBook(ActionEvent actionEvent) {
+    public void addNewBook() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fct/cs/addbook.fxml"));
             VBox box = loader.load();
@@ -163,5 +232,14 @@ public class MainBookController implements Initializable {
 
             System.out.println("close");
         }
+    }
+
+    public void searchTableFromText(String key) {
+        System.out.println("Searching ...");
+        bookFilteredList.setPredicate(bookData -> {
+            String filter = key.toLowerCase();
+            boolean nameMatches = String.valueOf(bookData.getTitle()).toLowerCase().contains(filter);
+            return nameMatches;
+        });
     }
 }
