374ff84eb7f8ad4cf43d4c599db2be59db66a1c0
S.Haturusinghe <haturusingh3@gmail.com>
2021-09-20 22:16:17 +0530

    minor changes

--- src/main/java/fct/cs/dbUtil/DatabaseHandler.java
+++ src/main/java/fct/cs/dbUtil/DatabaseHandler.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DatabaseHandler {
     private static String USER = "root";
     private static String PASS = "";
-    private static String DB_URL = "jdbc:mysql://localhost:3306/g05_bookstore";
+    private static String DB_URL = "jdbc:mysql://localhost:3306/group5_bookstore";
 
     private static DatabaseHandler instance;
 
@@ -14,17 +14,6 @@ public class DatabaseHandler {
 
     private DatabaseHandler() throws SQLException {
         createConnection();
-    }
-
-    private DatabaseHandler(String user, String pass, String url) {
-        changeDbInfo(user,pass,url);
-        createConnection();
-    }
-
-    private static void changeDbInfo(String user, String pass , String url){
-        USER = user;
-        PASS = pass;
-        DB_URL = "jdbc:mysql://" + url;
     }
 
     public static DatabaseHandler getInstance() throws SQLException {
@@ -37,14 +26,9 @@ public class DatabaseHandler {
         return instance;
     }
 
-    public static DatabaseHandler getInstance(String user, String pass, String url) throws SQLException {
-        instance = new DatabaseHandler(user, pass, url);
-        return instance;
-    }
-
-    public void createConnection()  {
+    public void createConnection() {
         try {
-            this.conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            this.conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Connection Created xoxo");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -55,7 +39,7 @@ public class DatabaseHandler {
         return conn;
     }
 
-    public ResultSet excecuteQuery(String query)  {
+    public ResultSet excecuteQuery(String query) {
         ResultSet resultSet;
         try {
             stmt = conn.createStatement();
@@ -68,7 +52,7 @@ public class DatabaseHandler {
         }
     }
 
-    public void closeConnection(){
+    public void closeConnection() {
         try {
             conn.close();
             System.out.println("Connection Closed");
